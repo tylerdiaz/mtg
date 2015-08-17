d316aa81bfc34406166035237f18184612bf78ea
Tyler J. Diaz <me@tylerdiaz.com>
2015-08-17 20:45:58 +0200

    ready to deploy!

--- Gemfile
+++ Gemfile
@@ -37,4 +37,9 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+gem "pry"
 
+gem "therubyracer"
+gem "less-rails"
+
+gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootstrap-rails.git'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,29 @@
+require 'net/http'
+require 'uri'
+
 class HomeController < ApplicationController
+  def index
+  end
+
+  def search
+    price = begin
+              card_name = params[:q].clone
+              card_name.gsub! " ", "+"
+              json = JSON.load(open_url("http://www.mtgprice.com/cardNameSearch?name=#{card_name}"))
+              puts "http://www.mtgprice.com/cardNameSearch?name=#{card_name}"
+              last_price = json.map { |card_data| card_data["lowestPrice"] }.max_by{ |k,v| v }
+            rescue
+              0.0
+            end
+
+    render json: {
+             n: params[:q],
+             v: price.to_f * (params[:amount] ? params[:amount].to_f : 1.0),
+             a: (params[:amount] ? params[:amount].to_i : 1)
+           }
+  end
+
+  def open_url(url)
+    Net::HTTP.get(URI.parse(url))
+  end
 end
